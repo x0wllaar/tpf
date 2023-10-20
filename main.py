7af725fd2cb5c41543bb5e0dbf857db9fb3d7b2e
Grigorii Khvatskii <gkhvatsk@nd.edu>
2023-10-20 19:40:25 -0400

    fix initial scaffolding

--- main.py
+++ main.py
@@ -9,33 +9,33 @@ def main(ctx: click.Context):
 
 
 @main.group("service")
-@click.option("--keyfile", type=click.Path(), required=True)
+@click.option("--keyfile", type=click.Path(), required=False)
 @click.pass_context
 def service_cmd(ctx: click.Context, keyfile: str):
     ctx.ensure_object(dict)
-    ctx.obj["KEYFILE"] = str(keyfile)
 
 
 @service_cmd.command("generate")
 @click.pass_context
 def service_keygen_cmd(ctx: click.Context):
     ctx.ensure_object(dict)
-    keyfile = ctx.obj["KEYFILE"]
 
 
 @service_cmd.command("serve")
 @click.option('--detached/--no-detached', default=False)
-@click.option('--clientkeys', type=click.Path(exists=True))
+@click.option('--clientkeyfile', type=click.Path(exists=True))
+@click.option("--clientkey", multiple=True)
 @click.argument('fwd_spec', nargs=-1, required=True)
 @click.pass_context
-def service_serve_cmd(ctx: click.Context, detached: bool, clientkeys: str, fwd_spec: Iterable[str]):
+def service_serve_cmd(ctx: click.Context, detached: bool, clientkeyfile: str,
+                      clientkey: Iterable[str], fwd_spec: Iterable[str]):
     ctx.ensure_object(dict)
-    keyfile = ctx.obj["KEYFILE"]
 
 
 @service_cmd.command("stop")
+@click.option("--serviceid")
 @click.pass_context
-def service_stop_cmd(ctx: click.Context, detached: bool, fwd_spec: Iterable[str]):
+def service_stop_cmd(ctx: click.Context, serviceid: str):
     ctx.ensure_object(dict)
     keyfile = ctx.obj["KEYFILE"]
 
